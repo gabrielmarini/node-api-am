81de2ceaaf8675d02b29c574dcbabd70e1ca1c66
Gabriel Marini <gabriel.g.marini@hotmail.com>
2017-11-04 18:51:56 -0200

    correção de erro no console e execução melhorada

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ import setupApp from './app'
 
 const port = 3000
 
-setupApp().then(app => app.listen(port, () => console.log('app running on port ${port}')))
+setupApp().then(app => app.listen(port, () => console.log('app running on port ' + port)))
 .catch(error => {
     console.error(error);
     process.exit(1);
